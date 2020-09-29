630d3467841095274cccef301efca757e12e4f40
VladSobko <vldsbk11@gmail.com>
2020-09-29 19:58:25 +0300

    made lab 1

---
+++ LR1ASP/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using LR1ASP.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace LR1ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        StudentMarksContext db = new StudentMarksContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Student> students = db.Students;
+            ViewBag.Students = students;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ChangeMark(int id)
+        {
+            ViewBag.StudentId = id;
+            return View();
+        }
+    }
+
+}
---
+++ LR1ASP/Models/GradeDbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+namespace LR1ASP.Models { 
+    public class GradeDbInitializer : DropCreateDatabaseAlways<StudentMarksContext> { 
+        protected override void Seed(StudentMarksContext db) { 
+            db.Students.Add(new Student { 
+                Name = "Владислав", 
+                Surname = "Собко", 
+                Group = "ІПЗс-19-1",
+                Course = 2,
+                English = 5,
+                Math = 4,
+                WebProgramming = 5
+            });
+            db.Students.Add(new Student {
+                Name = "Джонні",
+                Surname = "Депп",
+                Group = "ІПЗ-17-1",
+                Course = 4,
+                English = 3,
+                Math = 4,
+                WebProgramming = 3
+            }); 
+            db.Students.Add(new Student {
+                Name = "Бред",
+                Surname = "Пітт",
+                Group = "ІПЗс-18-1",
+                Course = 3,
+                English = 5,
+                Math = 3,
+                WebProgramming = 4
+            }); 
+            base.Seed(db); 
+        } 
+    } 
+}
